cbba7c7e6cf8543510ec678dccd9df365a27c97a
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-12-06 14:49:42 -0500

    Added screenshot

--- index.php
+++ index.php
@@ -29,22 +29,36 @@
     <article>
         <h1 id="gameName"></h1>
         <h3 id="groupName"></h3>
-        <h3>Instructions:</h3>
-        <div id="gameDesc" class="well">
-	  <ul>
-	    <li>Choose a corner chess board of any size.</li>
-	    <li>Both players initially place their kings in the board.</li>
-	    <li>Then proceed to play using normal chess rules.</li>
-	  </ul>
-        </div>
+        <h3>Rules</h3>
+        <ul class="list-group">
+          <li class="list-group-item">
+            At each ply, the player can either bring a new piece into play, 
+            or move an existing piece in the board.
+          </li>
+          <li class="list-group-item">
+            The first move for each player (white and black), must be bringing the King into play.
+          </li>
+          <li class="list-group-item">
+            Pieces being brought into play cannot be placed over an existing piece.
+          </li>
+          <li class="list-group-item">
+            White can only check black after black has checked or at ply 4 (whichever is earlier).
+           </li>
+          <li class="list-group-item">
+            Black cannot check white before ply 2.
+          </li>
+        </ul>
+
         <form id="gameSettings" class="well">
         </form>
-        <iframe src="games/corner_chess/iframe.html" class="game" width="800px" height="800px"></iframe>
+	<h3 style="text-align:center">Screenshot</h3>
+	<img src="games/corner_chess/img/corner_chess.png" width="100%" heigth="100%" />
+<!--        <iframe src="games/corner_chess/iframe.html" class="game" width="800px" height="800px"></iframe> -->
     </article>
     <?php include $base."footer.php"; ?>
 </div>
 <script type="text/javascript">
-    newWindowBtn(800,800,"games/corner_chess/iframe.html", []);
+    newWindowBtn(2000,2000,"games/corner_chess/iframe.html", []);
 </script>
 </body>
 </html>
